33d07de82aa01b6208fe942205ef89378501162c
Hasegawa Raku <raku@exhibition.jp>
2024-12-15 16:40:13 +0900

    Update build scripts

--- crates/canvaspads-web/build+optimize.sh
+++ crates/canvaspads-web/build+optimize.sh
@@ -1,2 +1,3 @@
 CARGO_TARGET_DIR="target" cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen ./target/wasm32-unknown-unknown/release/canvaspads_web.wasm --target web --out-dir ../../web/src/wasm
+wasm-opt -Oz -o ../../web/src/wasm/canvaspads_web_bg.wasm ../../web/src/wasm/canvaspads_web_bg.wasm
