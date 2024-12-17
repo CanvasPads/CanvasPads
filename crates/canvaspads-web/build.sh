11fe259fce880861e581f62b1a7c72919848615e
Hasegawa, Raku <raku@exhibition.jp>
2024-12-17 20:06:16 +0900

    Initialize wgpu renderer (#3)
    * Add
    
    * Update build scripts
    
    * Create wgpu driver
    
    * Update Cargo.toml
    
    * Remove wasm dists

--- crates/canvaspads-web/build.sh
+++ crates/canvaspads-web/build.sh
@@ -1,3 +1,2 @@
-CARGO_TARGET_DIR="target" cargo build --target wasm32-unknown-unknown --release
-wasm-opt -Oz -o ./target/wasm32-unknown-unknown/release/canvaspads_web.wasm ./target/wasm32-unknown-unknown/release/canvaspads_web.wasm
-wasm-bindgen ./target/wasm32-unknown-unknown/release/canvaspads_web.wasm --target web --out-dir ../../web/src/wasm
+CARGO_TARGET_DIR="target" cargo build --target wasm32-unknown-unknown
+wasm-bindgen ./target/wasm32-unknown-unknown/debug/canvaspads_web.wasm --target web --out-dir ../../web/src/wasm
